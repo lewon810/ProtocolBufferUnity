7def2bcf9d6f69285e228b494402b55f923cfc1e
Masatoki Mukai <lewon810@gmail.com>
2017-09-02 21:32:52 +0900

    initial commit

---
+++ ProtoBuffTest/Assets/TestScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Google.Protobuf;
+
+using System.IO;
+
+public class TestScene : MonoBehaviour {
+
+    [SerializeField]
+    UnityEngine.UI.Text _label;
+
+	// Use this for initialization
+	void Start () {
+        var person = new Person();
+        person.Id = 1;
+        person.Name = "だいき";
+
+        //scerialize
+        byte[] ouputBytes = person.ToByteArray();
+
+        //descerialize
+        Person person2 = new Person();
+		person2 = Person.Parser.ParseFrom(ouputBytes);
+
+        //log
+        _label.text = person2.ToString();
+
+	}
+
+    private Person TestFunc()
+    {
+        return new Person();
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
